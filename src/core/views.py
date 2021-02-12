843fead686684f0a6df6b5766092b03e949a8e8a
Tobi <Adeniyisoftware@gmail.com>
2021-02-12 19:00:13 +0000

    Update DRF TestView response

--- src/core/views.py
+++ src/core/views.py
@@ -1,8 +1,14 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+# 3rd Party Imports
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
-def test_view(request):
-    data = {'name': 'name', 'age': 23}
-    return JsonResponse(data)
+
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {'name': 'Dave', 'age': 25}
+        return Response(data)
--- src/django_api/urls.py
+++ src/django_api/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import test_view
+from core.views import TestView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', test_view, name='test'),
+    path('', TestView.as_view(), name='test'),
 ]
