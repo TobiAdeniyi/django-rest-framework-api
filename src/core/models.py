70fb49c871db7de5b4c29fd540832d78a747f120
Tobi <Adeniyisoftware@gmail.com>
2021-02-12 18:24:32 +0000

    Create Model for Products

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+# Store Products
+class Products(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    weight = models.FloatField(null=True)
+    color = models.CharField(null=True)
+    euler_char = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,8 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def test_view(request):
+    data = {'name': 'name', 'age': 23}
+    return JsonResponse(data)
