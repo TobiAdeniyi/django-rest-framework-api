1fbab9af9270d95e8d96ba982e9008538065caee
Tobi <Adeniyisoftware@gmail.com>
2021-02-16 16:23:26 +0000

    Create Product Serializer

--- src/core/views.py
+++ src/core/views.py
@@ -1,14 +1,28 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+from django.http import Http404
 
 # 3rd Party Imports
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-# Create your views here.
+# Model imports
+from core.models import Products
+from core.serializers import ProductSerializers
 
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
         data = {'name': 'Dave', 'age': 25}
         return Response(data)
+
+
+class ProductsList(APIView):
+    """
+    View to list all products in the stored
+    """
+
+    def get(self, request, format=None):
+        """
+        Returns a list of all products
+        """
+        products = []
