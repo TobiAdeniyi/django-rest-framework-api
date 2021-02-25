11fb2a7f999ef14b8daadfe970eead05b69fd8a8
Tobi <Adeniyisoftware@gmail.com>
2021-02-25 14:01:11 +0000

    FEAT: Create admin register for all models

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Products, Baskets, Customers
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Baskets)
+class BasketsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Customers)
+class CustomersAdmin(admin.ModelAdmin):
+    pass
